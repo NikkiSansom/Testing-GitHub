cf40f864e73cf1fdfb6bacc3015e667db1c24bb3
NikkiSansom <dsansom534@gmail.com>
2024-03-17 11:41:25 +0000

    added defensive programming

--- main.py
+++ main.py
@@ -15,7 +15,17 @@ def calculation(x, y, operator):
 num1 = float(input("Please enter a number: "))
 num2 = float(input("Please enter a number: "))
 
-operator = input("Please enter the operation symbol you'd like to use (+, -, * or /): ")
+ops = ["+", "-", "*", "/"]
+while True:
+    operator = input("Please enter the operation symbol you'd like to use (+, -, * or /): ")
+
+    if operator not in ops:
+        print("""Your selected operator was not valid.
+            Please try again.""")
+        continue
+    else:
+        print("Calculating ...")
+        break
 
 calc = calculation(num1, num2, operator)
 print(calc)
